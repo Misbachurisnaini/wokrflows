e3a109aa3bac2c3fe059d00c10f2633fa7693709
Misbachurisnaini <aninisnaini214@gmail.com>
2022-05-27 12:02:15 +0700

    looping 1

---
+++ loopingfor.dart
@@ -0,0 +1,18 @@
+void main() {
+  for (var angka = 0; angka <= 10; angka++) {
+    if (angka >= 0) {
+      angka += 1;
+      print(angka.toString() + (" Haechan"));
+    }
+
+    if (angka >= 1) {
+      angka += 1;
+      print(angka.toString() + (" Jaemin"));
+    }
+
+    if (angka >= 1) {
+      angka += 1;
+      print(angka.toString() + (" Jeno"));
+    }
+  }
+}
